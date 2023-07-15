693bf0e3c6fb070d389c4459e8da7b10dee4f4e2
Harsh Sharma <138365233+harsh-j-sharma@users.noreply.github.com>
2023-07-16 00:37:33 +0530

    595. Big Countries.sql

--- 595. Big Countries.sql
+++ 595. Big Countries.sql
@@ -28,3 +28,24 @@
 Select name, population, area
 from world
 where population > 25000000 OR area>3000000
+
+-----------------------or
+select name, population, area from World
+ where area>=3000000 
+ union 
+ select name, population, area from World 
+ where population>=25000000
+
+---The given query selects the `name`, `population`, and `area` columns from the `World` table. It combines two separate queries using the `UNION` operator to retrieve the countries that meet either of the following conditions:
+
+---1. Countries with an area greater than or equal to 3,000,000 (3 million):
+  ---- select name, population, area from World where area>=3000000
+  
+
+---2. Countries with a population greater than or equal to 25,000,000 (25 million):
+  ---select name, population, area from World where population>=25000000
+ 
+
+---The `UNION` operator combines the results of these two queries and removes any duplicate rows, giving the final result set of big countries that satisfy either the area or population condition.
+
+
