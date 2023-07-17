84e0599dd0b332f9a2f2342f71b871abd101dbf3
Harsh Sharma <138365233+harsh-j-sharma@users.noreply.github.com>
2023-07-18 00:09:26 +0530

    619. Biggest Single Number.sql

---
+++ 619. Biggest Single Number.sql
@@ -0,0 +1,56 @@
+-- Question 24
+-- Table my_numbers contains many numbers in column num including duplicated ones.
+-- Can you write a SQL query to find the biggest number, which only appears once.
+
+-- +---+
+-- |num|
+-- +---+
+-- | 8 |
+-- | 8 |
+-- | 3 |
+-- | 3 |
+-- | 1 |
+-- | 4 |
+-- | 5 |
+-- | 6 | 
+-- For the sample data above, your query should return the following result:
+-- +---+
+-- |num|
+-- +---+
+-- | 6 |
+-- Note:
+-- If there is no such number, just output null.
+
+-- Solution
+-- Question 24
+-- Table my_numbers contains many numbers in column num including duplicated ones.
+-- Can you write a SQL query to find the biggest number, which only appears once.
+
+-- +---+
+-- |num|
+-- +---+
+-- | 8 |
+-- | 8 |
+-- | 3 |
+-- | 3 |
+-- | 1 |
+-- | 4 |
+-- | 5 |
+-- | 6 | 
+-- For the sample data above, your query should return the following result:
+-- +---+
+-- |num|
+-- +---+
+-- | 6 |
+-- Note:
+-- If there is no such number, just output null.
+
+-- Solution
+
+SELECT MAX(num) AS largest_single_number
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(*) = 1
+) AS unique_numbers;
